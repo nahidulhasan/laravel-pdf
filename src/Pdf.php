1d7213efcc014ce8ee922f52604a326b4c36722d
Nahidul Hasan <nahidul.cse@gmail.com>
2019-03-09 16:42:23 +0600

    Code Refactored and removed generated pdf file

--- src/Pdf.php
+++ src/Pdf.php
@@ -29,16 +29,17 @@ class Pdf
 
         if (shell_exec("xvfb-run wkhtmltopdf {$htmlFile} {$pdfFile}")) {
 
-            return file_get_contents($pdfFile);
+            $file = file_get_contents($pdfFile);
+
+            return $this->removeAndReturnFile($htmlFile, $pdfFile, $file);
 
         } elseif (shell_exec("wkhtmltopdf {$htmlFile} {$pdfFile}")) {
 
-            return file_get_contents($pdfFile);
+            $file = file_get_contents($pdfFile);
+
+            return $this->removeAndReturnFile($htmlFile, $pdfFile, $file);
 
         }
-
-       // shell_exec("rm {$htmlFile}");
-       // shell_exec("rm {$pdfFile}");
 
     }
 
@@ -46,20 +47,16 @@ class Pdf
     /**
      * Make the PDF downloadable by the user
      *
+     * @param $input
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function download()
+    public function download($input)
     {
-        $file = public_path(). "/page.pdf";
-
-        if (!is_file($file)) {
-            echo("404 File not found!");
-            exit();
-        }
+        $file = $this->generatePdf($input);
 
         $headers = [
             'Content-Type' => 'application/pdf',
-            'Content-Disposition' =>  'attachment; filename="'.'filename.pdf'.'"',
+            'Content-Disposition' => 'attachment; filename="' . 'filename.pdf' . '"',
         ];
 
         return response()->download($file, 'filename.pdf', $headers);
@@ -68,21 +65,33 @@ class Pdf
 
     /**
      * Open PDF in the browser
+     * @param $input
      */
-    public function stream()
+    public function stream($input)
     {
-        $file = public_path(). "/page.pdf";
-
-        if (!is_file($file)) {
-            echo("404 File not found!");
-            exit();
-        }
+        $file = $this->generatePdf($input);
 
         header('Content-type: application/pdf');
         header('Content-Disposition: inline; filename="' . 'filename.pdf' . '"');
         header('Content-Transfer-Encoding: binary');
         header('Accept-Ranges: bytes');
-        echo file_get_contents($file);
+        echo $file;
 
+    }
+
+    /**
+     * Removed generated file and return
+     *
+     * @param $htmlFile
+     * @param $pdfFile
+     * @param $file
+     * @return mixed
+     */
+    public function removeAndReturnFile($htmlFile, $pdfFile, $file)
+    {
+        shell_exec("rm {$htmlFile}");
+        shell_exec("rm {$pdfFile}");
+
+        return $file;
     }
 }
